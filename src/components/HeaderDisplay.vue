42cdb1c7927bbd5f12e2b36ede6f79f55a4bbbed
ditro74 <ditro74@naver.com>
2022-02-16 15:25:15 +0900

    first commit

---
+++ src/components/HeaderDisplay.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <h2>Weather Application</h2>
+    <p>{{ username }} 님 반갑습니다.</p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderDisplay",
+};
+</script>
+
+<style scoped>
+.header {
+  background: linear-gradient(15deg, #8a2387, #e94057, #f27121);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em; /* 1em = 16px */
+}
+.header h2 {
+  color: #fff;
+}
+.header p {
+  font-size: 12px;
+  color: #fff;
+}
+</style>
---
+++ src/components/weather/CitySelector.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="test">
+    <ul class="city-selector">
+      <li
+        @click="selectCity(city)"
+        :class="{ isSelected: city.selected }"
+        v-for="city in cities"
+        :key="city.code"
+      >
+        {{ city.label }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CitySelector",
+  data() {
+    return {
+      cities: [
+        { code: "Seoul", label: "서울", selected: false },
+        { code: "Tokyo", label: "도쿄", selected: false },
+        { code: "London", label: "런던", selected: false },
+        { code: "Moscow", label: "모스크바", selected: false },
+        { code: "Taiwan", label: "대만", selected: false },
+        { code: "Ukraine", label: "우쿠라이나", selected: false },
+        { code: "Washington", label: "워싱턴", selected: false },
+        { code: "New york", label: "뉴욕", selected: false },
+      ],
+    };
+  },
+  methods: {
+    selectCity(city) {
+      city.selected = !city.selected;
+      this.$emit("selectCity", city);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.city-selector {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+}
+.city-selector li {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 0.05em solid #efefef;
+  padding: 0.3em;
+  cursor: pointer;
+}
+.isSelected {
+  background: #fd1d1d;
+  color: #fff;
+}
+</style>
